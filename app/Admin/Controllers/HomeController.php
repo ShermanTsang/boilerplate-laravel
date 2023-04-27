5ef1f02b437bfd987fd9c0a4ec08e9748cf8f48d
ShermanT <sharemant@163.com>
2023-04-27 16:11:24 +0800

    chore: clean useless business files

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -13,10 +13,7 @@ class HomeController extends Controller
     {
         $tab = Tab::make();
         $docs = [
-            ['name' => '文档概述', 'url' => 'https://www.yuque.com/shareman/tgtsea/la8q9vi1og40ngp8?singleDoc#', 'isActive' => true],
-            ['name' => '功能需求', 'url' => 'https://www.yuque.com/shareman/tgtsea/sabf7labfc1l28t2?singleDoc#', 'isActive' => false],
-            ['name' => '项目架构', 'url' => 'https://www.yuque.com/shareman/tgtsea/st5t076xgveygu9o?singleDoc#', 'isActive' => false],
-            ['name' => '教程文档', 'url' => 'https://www.yuque.com/shareman/tgtsea/fgpg8ts9gw4rol0a?singleDoc#', 'isActive' => false],
+//            ['name' => '文档概述', 'url' => '', 'isActive' => true],
         ];
         foreach ($docs as $doc) {
             $tab->add($doc['name'], view('iframe', ['name' => $doc['name'], 'url' => $doc['url']]), $doc['isActive']);

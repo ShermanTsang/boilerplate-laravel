b62c6dd1e2581e02f39595e1f9cc6cd30249b1f8
ShermanTsang <isherman@163.com>
2025-05-25 13:05:24 +0800

    feat: modify custom datetime field template

--- resources/views/datetime.blade.php
+++ resources/views/datetime.blade.php
@@ -1,13 +1,14 @@
 @if ($value)
     @php
-        $carbonTime = new Carbon\Carbon($value)
+        $carbonTime = new Carbon\Carbon($value);
+        $localtime = $carbonTime->setTimezone('Asia/Shanghai');
     @endphp
     <div class="datetime-column">
         <div><span
-                class="datetime-column-region">大陆</span>{{ $carbonTime->setTimezone('Asia/Shanghai')->toDateTimeString()  }}
+                class="datetime-column-region">具体</span>{{ $localtime->toDateTimeString()  }}
         </div>
         <div><span
-                class="datetime-column-region">日本</span>{{ $carbonTime->setTimezone('Asia/Tokyo')->toDateTimeString() }}
+                class="datetime-column-region">已读</span>{{ $localtime->diffForHumans() }}
         </div>
     </div>
 @endif
